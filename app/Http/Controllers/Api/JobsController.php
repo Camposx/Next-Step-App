4b5e4f01b2fef482dd1498b4cfbf523d70c01558
Camposx <alejandrocamposgon@gmail.com>
2024-11-26 16:12:38 +0100

    feat: create api update method

--- app/Http/Controllers/Api/JobsController.php
+++ app/Http/Controllers/Api/JobsController.php
@@ -14,11 +14,6 @@ class JobsController extends Controller
         return response()->json($jobs, 200);
     }
 
-    public function create()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
         $jobs = Jobs::create([
@@ -37,15 +32,30 @@ class JobsController extends Controller
         return response()->json($jobs, 200);
     }
 
-    public function show(string $id)
+    public function show(string $job_id)
     {
-        $jobs = Jobs::find($id);
+        $jobs = Jobs::find($job_id);
         return response()->json($jobs, 200);
     }
 
     public function update(Request $request, string $id)
     {
+        $jobs = Jobs::find($id);
 
+        $jobs->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'company_name' => $request->company_name,
+            'job_portal' => $request->job_portal,
+            'work_type' => $request->work_type,
+            'work_mode' => $request->work_mode,
+            'salary_range' => $request->salary_range,
+            'notes' => $request->notes,
+            'status' => $request->status
+        ]);
+
+        $jobs->save();
+        return response()->json($jobs, 200);
     }
 
 
