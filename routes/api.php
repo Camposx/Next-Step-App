02e318c09f853880a3841917af52233e9cd51632
Camposx <alejandrocamposgon@gmail.com>
2024-11-26 12:35:54 +0100

    feat: add api routes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/jobs' ,[JobsController::class, 'index'])->name('apihome');
+Route::get('/jobs' ,[JobsController::class, 'store'])->name('apistore');
+Route::get('/jobs/{id}' ,[JobsController::class, 'destroy'])->name('apidestroy');
+Route::get('/jobs/{id}' ,[JobsController::class, 'update'])->name('apiupdate');
+Route::get('/jobs/{id}' ,[JobsController::class, 'store'])->name('apistore');
