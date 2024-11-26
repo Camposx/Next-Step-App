e93aef1654db542a94e1913af56075814c954e96
Camposx <alejandrocamposgon@gmail.com>
2024-11-26 11:50:28 +0100

    feat: api getall method in controller + api route

--- app/Http/Controllers/Api/JobsController.php
+++ app/Http/Controllers/Api/JobsController.php
@@ -2,10 +2,47 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Jobs;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class JobsController extends Controller
 {
-    //
+    public function index()
+    {
+        $jobs = Jobs::all();
+        return response()->json($jobs, 200);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, string $id)
+    {
+
+    }
+
+
+    public function destroy(string $id)
+    {
+
+    }
 }
