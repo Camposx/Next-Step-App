3b56f3051c5e80ac818e59a360e922b52fd65796
Camposx <alejandrocamposgon@gmail.com>
2024-11-28 11:09:28 +0100

    feat: add index function to blade controller

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class JobsController extends Controller
 {
-    //
+    public function index()
+    {
+        $jobs = Jobs::all();
+        return view("home", compact("jobs"));
+    }
 }
