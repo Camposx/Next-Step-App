efdde9e0f1ea36b79e94822271d192e1ac7c88f0
Camposx <alejandrocamposgon@gmail.com>
2024-11-28 14:57:48 +0100

    fix: table name incorrect

--- app/Models/Jobs.php
+++ app/Models/Jobs.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Jobs_tracking;
 
 class Jobs extends Model
 {
     use HasFactory;
+
+    public function Jobs_tracking(){
+        return $this->hasMany(Jobs_Tracking::class);
+    }
 
     protected $table = 'jobs';
 
--- app/Models/jobs_tracking.php
+++ app/Models/jobs_tracking.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use App\Models\Jobs;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Jobs_tracking extends Model
 {
+    use HasFactory;
     //
     public function Jobs(){
         return $this->belongsTo(Jobs::class);
