7102a790191bc703bf3a55f3415d31d1001c17b0
Camposx <alejandrocamposgon@gmail.com>
2024-11-28 12:58:19 +0100

    fix: migration foreign

--- database/migrations/2024_11_26_085857_create_jobs_trackings_table.php
+++ database/migrations/2024_11_26_085857_create_jobs_trackings_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('jobs_trackings', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('job_id');
+            $table->foreign('job_id')->references('id')->on('jobs');
             $table->string('step_name');
             $table->text('notes');
             $table->string('track_status');
