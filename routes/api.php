6a510c1f5986432917981fbfced0673b629472db
Camposx <alejandrocamposgon@gmail.com>
2024-12-05 13:16:16 +0100

    fix: routes camelCase fix

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\JobsController;
 use App\Http\Controllers\Api\JobTrackingController;
 
-Route::get('/jobs' ,[JobsController::class, 'index'])->name('apihome');
-Route::post('/jobs' ,[JobsController::class, 'store'])->name('apistore');
-Route::delete('/jobs/{id}' ,[JobsController::class, 'destroy'])->name('apidestroy');
-Route::put('/jobs/{id}' ,[JobsController::class, 'update'])->name('apiupdate');
-Route::get('/jobs/{id}' ,[JobsController::class, 'show'])->name('apishow');
+Route::get('/jobs' ,[JobsController::class, 'index'])->name('apiHomeJob');
+Route::post('/jobs' ,[JobsController::class, 'store'])->name('apiStoreJob');
+Route::delete('/jobs/{id}' ,[JobsController::class, 'destroy'])->name('apiDestroyJob');
+Route::put('/jobs/{id}' ,[JobsController::class, 'update'])->name('apiUpdateJob');
+Route::get('/jobs/{id}' ,[JobsController::class, 'show'])->name('apiShowJob');
 
-Route::get('/job-tracker' ,[JobTrackingController::class, 'index'])->name('apihomeTrack');
-Route::post('/job-tracker' ,[JobTrackingController::class, 'store'])->name('apistoreTrack');
-Route::delete('/job-tracker/{id}' ,[JobTrackingController::class, 'destroy'])->name('apidestroyTrack');
-Route::put('/job-tracker/{id}' ,[JobTrackingController::class, 'update'])->name('apiupdateTrack');
-Route::get('/job-tracker/{id}' ,[JobTrackingController::class, 'show'])->name('apishowTrack'); 
+Route::get('/job-tracker' ,[JobTrackingController::class, 'index'])->name('apiHomeTrack');
+Route::post('/job-tracker' ,[JobTrackingController::class, 'store'])->name('apiStoreTrack');
+Route::delete('/job-tracker/{id}' ,[JobTrackingController::class, 'destroy'])->name('apiDestroyTrack');
+Route::put('/job-tracker/{id}' ,[JobTrackingController::class, 'update'])->name('apiUpdateTrack');
+Route::get('/job-tracker/{id}' ,[JobTrackingController::class, 'show'])->name('apiShowTrack'); 
