6b59ad0a573c7d17faf9c8b4dace84fdb811ab49
Camposx <alejandrocamposgon@gmail.com>
2024-12-10 20:08:09 +0100

    style: main list styles done

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>©️Copyright by Alejandro Campos</p>
+    <p>©️ Copyright by Alejandro Campos</p>
 </footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,44 @@
         @foreach($jobs as $job)
             <a class="link" href="{{ route('showJobDetail', ['id' => $job->id]) }}">
                 <div class="jobOffer">
-                    <div>
-                        <div class="title">
-                            <h3>{{$job->title}}</h3>
+                        <div class="offerContent">
+                            <div class="offerHeading">
+                                <h3>{{$job->title}}</h3>
+                                <p>Applied: {{$job->created_at->format('d/m/Y')}}</p>
+                            </div>
+                            <div class="offerBody">
+                                <div class="company">
+                                    <p>Company:</p>
+                                    <h5>{{$job->company_name}}</h5>
+                                </div>
+                                <div class="field">
+                                    <p>Salary range: </p>
+                                    <h5>{{$job->salary_range}}</h5>
+                                </div>
+                                <div class="field">
+                                    <p>Work Type: </p>
+                                    <h5>{{$job->work_type}}</h5>
+                                </div>
+                                <div class="field">
+                                    <p>Work Mode </p>
+                                    <h5>{{$job->work_mode}}</h5>
+                                </div>
+                                <div class="field">
+                                    <p>Portal: </p>
+                                    <h5>{{$job->job_portal}}</h5>
+                                </div>
+                            </div>
                         </div>
-                        <div class="shortDesc">
-                            <p>{{$job->description}}</p>
+                        <div class="offerStatus">
+                            <div class="statusDad">
+                                <div class="status">
+                                    <h5>{{$job->status}}</h5>
+                                </div>
+                            </div>
+                            <div class="lastUpdate">
+                                <p>Last update: {{$job->updated_at}}</p>
+                            </div>
                         </div>
-                        <div class="status">
-                            <h5>{{$job->status}}</h5>
-                        </div>
-                        <div class="dateTime">
-                            <h4>{{$job->created_at->format('d/m/Y')}}</h4>
-                        </div>
-                    </div>
                 </div>
             </a>
         @endforeach
